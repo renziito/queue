95ff1be62c1033a56f602a8172fdab810b388b3e
Renziito <sepia.aki@gmail.com>
2020-03-09 01:40:34 -0500

    install instructions and search

--- protected/modules/cmd/controllers/DbController.php
+++ protected/modules/cmd/controllers/DbController.php
@@ -25,8 +25,12 @@ class DbController extends Controller {
         }
     }
 
-    public function actionQuery() {
+    public function actionSearch() {
         $query = Yii::app()->request->getQuery('query');
+        $words = explode("|", $query);
+        $searchable = "'%" . implode("%' or  text like '%", str_replace('+', ' ', $words)) . "%'";
+        
+        echo Event::model()->count("state = 1 AND text like " . $searchable);
     }
 
 }
