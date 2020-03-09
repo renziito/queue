8ee07d2ea8a8bb2c096d905412605f64fda783b5
Renziito <sepia.aki@gmail.com>
2020-03-09 00:39:46 -0500

    test post

--- protected/modules/cmd/controllers/DbController.php
+++ protected/modules/cmd/controllers/DbController.php
@@ -6,10 +6,10 @@ class DbController extends Controller {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Access-Control-Allow-Headers: Authorization");
-        
+
         $event = new Event;
         Utils::show($_POST);
-        $event->attributes = json_decode($_POST);
+        $event->attributes = $_POST;
         echo json_encode($event->attributes);
         $event->save();
         echo json_encode($event->getErrors());
