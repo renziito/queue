5bb2245b6ae30a8457b81a6cb0b04053b36c56d8
Renziito <sepia.aki@gmail.com>
2020-03-09 00:53:15 -0500

    json phpinput

--- protected/modules/cmd/controllers/DbController.php
+++ protected/modules/cmd/controllers/DbController.php
@@ -6,11 +6,15 @@ class DbController extends Controller {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-        header('content-type: application/json; charset=utf-8');
-        
+
         $event = new Event;
-        Utils::show($_POST);
-        $event->attributes =$_POST;
+
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+
+
+        Utils::show($data);
+        $event->attributes = $data;
         echo json_encode($event->attributes);
         $event->save();
         echo json_encode($event->getErrors());
