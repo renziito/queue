4380b4abd25fae60a7f29dca6a88d0f380e393c3
Renziito <sepia.aki@gmail.com>
2020-03-09 00:58:11 -0500

    headers

--- protected/modules/cmd/controllers/DbController.php
+++ protected/modules/cmd/controllers/DbController.php
@@ -4,8 +4,8 @@ class DbController extends Controller {
 
     public function actionSaveEvent() {
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+        header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
         $event = new Event;
 
