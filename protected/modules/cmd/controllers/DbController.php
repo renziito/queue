36c8a7c2d85416c71a2db107c7ec331c4eabbf97
Renziito <sepia.aki@gmail.com>
2020-03-09 00:48:46 -0500

    json

--- protected/modules/cmd/controllers/DbController.php
+++ protected/modules/cmd/controllers/DbController.php
@@ -5,11 +5,12 @@ class DbController extends Controller {
     public function actionSaveEvent() {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-        header("Access-Control-Allow-Headers: Authorization");
-
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('content-type: application/json; charset=utf-8');
+        
         $event = new Event;
         Utils::show($_POST);
-        $event->attributes = $_POST;
+        $event->attributes =$_POST;
         echo json_encode($event->attributes);
         $event->save();
         echo json_encode($event->getErrors());
